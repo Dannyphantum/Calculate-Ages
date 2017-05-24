28238ad5d9c433cf4bd037825cfe8b8f095cd393
Your Name <youremail@domain.com>
2017-05-24 16:24:54 -0400

    First commit

---
+++ ageCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class ageCalculator{
+
+public static void main(String[] args){
+
+String userDOB = "";
+int userAge = 0;
+Calendar currentDate = new Calendar(userDOB);
+String  userChoice = "";
+
+System.out.println(currentDate);
+
+}
+
+}
