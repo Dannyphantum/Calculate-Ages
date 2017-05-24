cd335460716e8b75d98fd4099bc334984893ce2a
Your Name <youremail@domain.com>
2017-05-24 17:28:38 -0400

    Made some improvements

--- ageCalculator.java
+++ ageCalculator.java
@@ -5,12 +5,35 @@ public class ageCalculator{
 
 public static void main(String[] args){
 
-String userDOB = "";
+int userYear = 0;
+int userDay = 0;
+int userMonth = 0;
 int userAge = 0;
-Calendar currentDate = new Calendar(userDOB);
+Calendar today = Calendar.getInstance();
 String  userChoice = "";
 
-System.out.println(currentDate);
+do{
+	System.out.println("Enter Year of Birth");
+	Scanner yearScanner = new Scanner(System.in);
+	userYear = yearScanner.nextInt();
+
+	System.out.println("Enter Month of Birth");
+	Scanner monthScanner = new Scanner(System.in);
+	userMonth = monthScanner.nextInt();
+
+	System.out.println("Enter Day of Birth");
+	Scanner dayScanner = new Scanner(System.in);
+	userDay = dayScanner.nextInt();
+
+	//System.out.println(currentDate);
+	
+	System.out.println("Would you like to enter details about another client?");
+	Scanner choiceScanner = new Scanner(System.in);
+	userChoice = choiceScanner.next();
+
+}while(userChoice == "Y");
+
+System.out.println("Thanks for using the Age Calculator!");
 
 }
 
